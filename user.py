910eb0d0c4ef02d6e0b53c8e4fb514999090e5a6
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-06-24 13:54:39 +0530

    Update user.py

--- user.py
+++ user.py
@@ -7,7 +7,7 @@ BOT_USERNAME=Config.BOT_USERNAME
 class User(Client):
     def __init__(self):
         super().__init__(
-            name="past-file-forward-user",
+            Config.USER_SESSION,
             session_string=Config.SESSION,
             api_hash=Config.API_HASH,
             api_id=Config.API_ID,
